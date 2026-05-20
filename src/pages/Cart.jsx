31e6ed587daa75a2d8fbe62ca100c2f1408886ca
sonujustin2022 <sonujustin6282@gmail.com>
2026-05-20 22:32:10 +0530

    2 bttns added

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -80,6 +80,10 @@ const Cart = () => {
               </tbody>
             </table>
             <button onClick={()=>dispatch(emptyCart())} className="bg-red-600 text-1xl text-white rounded p-2 ms-2">Empty Cart</button>
+            
+            <button onClick={()=>dispatch(emptyCart())} className="bg-red-600 text-1xl text-white rounded p-2 ms-2">Empty Cart</button>
+            
+            <button onClick={()=>dispatch(emptyCart())} className="bg-red-600 text-1xl text-white rounded p-2 ms-2">Empty Cart</button>
           </div>
 
           {/* left side */}
